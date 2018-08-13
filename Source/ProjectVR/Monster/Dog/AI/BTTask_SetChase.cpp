d98d4b3569725922971b1e02f07a884861db3d65
지수 김 <rhlcp23@naver.com>
2018-08-13 10:20:10 +0900

    AI 간소화

--- Source/ProjectVR/Monster/Dog/AI/BTTask_SetChase.cpp
+++ Source/ProjectVR/Monster/Dog/AI/BTTask_SetChase.cpp
@@ -5,6 +5,9 @@
 #include "Monster/Dog/Dog.h"
 #include "Monster/Dog/DogAIController.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "MyCharacter/MotionControllerCharacter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Camera/CameraComponent.h"
 
 EBTNodeResult::Type UBTTask_SetChase::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
 {
@@ -13,13 +16,27 @@ EBTNodeResult::Type UBTTask_SetChase::ExecuteTask(UBehaviorTreeComponent & Owner
 	if (AI)
 	{
 		ADog* Dog = Cast<ADog>(AI->GetPawn());
+		AActor* Player = Cast<AActor>(AI->BBComponent->GetValueAsObject(TEXT("Player")));
+		AMotionControllerCharacter* MyCharacter = Cast<AMotionControllerCharacter>(Player);
+
+		Distance = FVector::Distance(Dog->GetActorLocation(), MyCharacter->Camera->GetComponentLocation());
 
 		if (Dog)
 		{
-			Dog->CurrentDogState = EDogState::Chase;
-			Dog->CurrentDogAnimState = EDogAnimState::Run;
-			Dog->CurrentDogJumpState = EDogJumpState::Nothing;
-			Dog->CurrentDogCircleState = EDogCircleState::Nothing;
+			if (Distance > 400.0f)
+			{
+				Dog->CurrentDogState = EDogState::Chase;
+				Dog->CurrentDogAnimState = EDogAnimState::Run;
+				Dog->CurrentDogJumpState = EDogJumpState::Nothing;
+				Dog->CurrentDogCircleState = EDogCircleState::Nothing;
+			}
+			else
+			{
+				Dog->CurrentDogState = EDogState::Battle;
+				Dog->CurrentDogAnimState = EDogAnimState::Nothing;
+				Dog->CurrentDogJumpState = EDogJumpState::Nothing;
+				Dog->CurrentDogCircleState = EDogCircleState::Nothing;
+			}
 		}
 	}
 	return EBTNodeResult::Succeeded;
