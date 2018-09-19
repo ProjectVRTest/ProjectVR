520f0702c4ab45fcdc9003c58a352bfb7647b551
지수 김 <rhlcp23@naver.com>
2018-09-19 09:54:21 +0900

    커스텀 버튼

---
+++ Source/ProjectVR/Widgets/CustomButton.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CustomButton.h"
+#include "UObject/ConstructorHelpers.h"
+#include "UMG.h"
+
+#define LOCTEXT_NAMESPACE "UMG"
+
+UCustomButton::UCustomButton()
+{
+	SButton::FArguments ButtonDefaults;
+
+	static ConstructorHelpers::FObjectFinder<USlateWidgetStyleAsset>CustomStyle(TEXT("SlateWidgetStyleAsset'/Game/Blueprints/UI/Button/CustomWidgetStyleAsset.CustomWidgetStyleAsset'"));
+	if (CustomStyle.Succeeded())
+	{
+		ButtonDefaults.ButtonStyle(CustomStyle.Object);
+	}
+	WidgetStyle = *ButtonDefaults._ButtonStyle;
+
+}
+
+#if WITH_EDITOR
+const FText UCustomButton::GetPaletteCategory()
+{
+	return LOCTEXT("", "My Custom Cuff");
+}
+#endif
+
+#undef LOCTEXT_NAMESPACE
